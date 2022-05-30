25e7bd1b7a6bf2da64b7c5884110b1efff2193b8
geekrope <geekrope@gmail.com>
2022-05-30 15:26:24 +0300

    Added client part. Added letter apear animation

--- server/app.js
+++ server/app.js
@@ -1,26 +1,20 @@
-import * as express from 'express';
-import { AddressInfo } from "net";
-import * as path from 'path';
-
-import routes from './routes/index';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path = require("path");
+const index_1 = require("./routes/index");
 const debug = require('debug')('my express app');
 const app = express();
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
+app.use(express.static(path.join(__dirname, 'routes')));
+app.use('/', index_1.default);
 app.use((_req, _res, next) => {
     const err = new Error('Not Found');
     next(err);
 });
-
 if (app.get('env') === 'development') {
-    app.use((err: any, _req: any, res: any, _next: any) => {
+    app.use((err, _req, res, _next) => {
         res.status(err['status'] || 500);
         res.render('error', {
             message: err.message,
@@ -28,18 +22,15 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
-
-app.use((err: any, _req: any, res: any, _next: any) => {
+app.use((err, _req, res, _next) => {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 app.set('port', process.env["PORT"] || 3000);
-
 const server = app.listen(app.get('port'), function () {
-    debug(`Express server listening on port ${ (server.address() as AddressInfo).port }`);
+    debug(`Express server listening on port ${server.address().port}`);
 });
+//# sourceMappingURL=app.js.map
