f994d3fb6bf1d62a0e7af8eb43052ca7bc860309
geekrope <geekrope@gmail.com>
2022-06-10 18:44:58 +0300

    Added keyboard

--- client/client/app.ts
+++ client/client/app.ts
@@ -7,6 +7,41 @@ const guessesId = "guesses";
 
 type letterType = "correct" | "present" | "absent";
 
+const keyboardRow1: { content: string | undefined, code: number, flex?: number }[] = [
+	{ content: undefined, code: 81 },
+	{ content: undefined, code: 87 },
+	{ content: undefined, code: 69 },
+	{ content: undefined, code: 82 },
+	{ content: undefined, code: 84 },
+	{ content: undefined, code: 89 },
+	{ content: undefined, code: 85 },
+	{ content: undefined, code: 73 },
+	{ content: undefined, code: 79 },
+	{ content: undefined, code: 80 }
+];
+const keyboardRow2: { content: string | undefined, code: number, flex?: number }[] = [
+	{ content: undefined, code: 65 },
+	{ content: undefined, code: 83 },
+	{ content: undefined, code: 68 },
+	{ content: undefined, code: 70 },
+	{ content: undefined, code: 71 },
+	{ content: undefined, code: 72 },
+	{ content: undefined, code: 74 },
+	{ content: undefined, code: 75 },
+	{ content: undefined, code: 76 }
+];
+const keyboardRow3: { content: string | undefined, code: number, flex?: number }[] = [
+	{ content: "ENTER", code: 13, flex: 1.5 },
+	{ content: undefined, code: 90 },
+	{ content: undefined, code: 88 },
+	{ content: undefined, code: 67 },
+	{ content: undefined, code: 68 },
+	{ content: undefined, code: 66 },
+	{ content: undefined, code: 78 },
+	{ content: undefined, code: 77 },
+	{ content: "@/backspace.svg", code: 8, flex: 1.5 }
+];
+
 let currentRow = 0;
 let currentColumn = 0;
 
@@ -19,6 +54,11 @@ interface UserStatistics
 	maxStreak: number;
 }
 
+function toUpper(code: number): number
+{
+	return String.fromCharCode(code).toUpperCase().charCodeAt(0);
+}
+
 function getLetterId(column: number, row: number): string
 {
 	return "letter" + column + "_" + row;
@@ -29,16 +69,27 @@ function getLetter(column: number, row: number): HTMLElement | null
 	return document.getElementById(getLetterId(column, row));
 }
 
+function getKeyId(code: number): string
+{
+	return "key_" + code;
+}
+
+function getKey(code: number): HTMLElement | null
+{
+	return document.getElementById(getKeyId(code));
+}
+
 function createRow(row: number): HTMLDivElement
 {
 	const container = document.createElement("div");
 	container.className = "flex rowContainer";
+	container.style.width = "calc(var(--size) * 330)";
 
 	for (let column = 0; column < lettersCount; column++)
 	{
 		const letterContainer = document.createElement("div");
 
-		letterContainer.className = "letterContainer unspecifiedLetter";
+		letterContainer.className = "innerCentralAlign letterContainer unspecifiedLetter";
 		letterContainer.id = getLetterId(column, row);
 
 		container.appendChild(letterContainer);
@@ -51,6 +102,7 @@ function createBoard(): HTMLDivElement
 {
 	const container = document.createElement("div");
 	container.className = "flex columnContainer";
+	container.style.height = "calc(var(--size) * 400)";
 
 	for (let row = 0; row < rowsCount; row++)
 	{
@@ -63,6 +115,68 @@ function createBoard(): HTMLDivElement
 function createBlankStatistics(): UserStatistics
 {
 	return { correctAnswers: 0, totalAnswers: 0, guessesDistribution: new Array<number>(rowsCount).fill(0), currentStreak: 0, maxStreak: 0 };
+}
+
+function createKey(content: string, action: () => void): HTMLDivElement
+{
+	const container = document.createElement("div");
+	container.className = "innerCentralAlign keyStyle";
+
+	if (content[0] == "@")
+	{
+		container.innerHTML = `<img src="${content.substring(1)}"></img>`;
+	}
+	else
+	{
+		container.innerHTML = content;
+	}
+
+	container.onclick = action;
+
+	return container;
+}
+
+function createKeyboardRow(row: { content: string | undefined, code: number, flex?: number }[]): HTMLDivElement
+{
+	const container = document.createElement("div");
+	container.className = "rowContainer";
+	container.style.display = "flex";
+	container.style.justifyContent = "center";
+	container.style.width = "calc(var(--key-size)*484)";
+	container.style.padding = "0 calc(var(--key-size)*8) 0 calc(var(--key-size)*8)";
+
+	row.forEach((value, index) =>
+	{
+		const key = createKey(value.content ? value.content : String.fromCharCode(value.code), () => { keyHandler(value.code) });
+
+		if (index < row.length - 1)
+		{
+			key.style.marginRight = "calc(var(--size) * 6)";
+		}
+		if (value.flex)
+		{
+			key.style.flex = value.flex.toString();
+		}
+
+		key.id = getKeyId(value.code);
+
+		container.appendChild(key);
+	});
+
+	return container;
+}
+
+function createKeyboard(): HTMLDivElement
+{
+	const container = document.createElement("div");
+	container.className = "flex columnContainer";
+	container.style.height = "200px";
+
+	container.appendChild(createKeyboardRow(keyboardRow1));
+	container.appendChild(createKeyboardRow(keyboardRow2));
+	container.appendChild(createKeyboardRow(keyboardRow3));
+
+	return container;
 }
 
 function getStatistics(): UserStatistics
@@ -83,12 +197,12 @@ function getStatistics(): UserStatistics
 	}
 }
 
-function setStatistics(stats: UserStatistics)
+function setStatistics(stats: UserStatistics): void
 {
 	localStorage.setItem(statisticsId, JSON.stringify(stats));
 }
 
-async function submitCurrentRow(): Promise<boolean | string>
+function getCurrentWord(): string
 {
 	let word = "";
 
@@ -102,49 +216,10 @@ async function submitCurrentRow(): Promise<boolean | string>
 		}
 	}
 
-	const text = await (await fetch(`/guess?word=${word}`, { method: "POST" })).text();
-
-	try
-	{
-		const specifiedWord = JSON.parse(text) as { type: letterType, _charCode: number }[];
-
-		let correctLetters = 0;
-
-		specifiedWord.forEach((value, index) =>
-		{
-			const letterElement = getLetter(index, currentRow);
-
-			if (letterElement)
-			{
-				switch (value.type)
-				{
-					case "correct":
-						letterElement.className = "letterContainer specifiedLetter specifiedCorrect";
-						correctLetters++;
-						break;
-					case "present":
-						letterElement.className = "letterContainer specifiedLetter specifiedPresent";
-						break;
-					case "absent":
-						letterElement.className = "letterContainer specifiedLetter specifiedAbsent";
-						break;
-					default:
-						throw new Error("Unknown letter type");
-				}
-
-				letterElement.style.animationDelay = index * animationDelay + "s";
-			}
-		});
-
-		return correctLetters == lettersCount;
-	}
-	catch
-	{
-		return text;
-	}
+	return word;
 }
 
-function winHandler()
+function winHandler(): void
 {
 	alert("biungo");
 
@@ -159,7 +234,7 @@ function winHandler()
 	setStatistics(userStats);
 }
 
-function loseHandler()
+function loseHandler(): void
 {
 	alert("jsdngiksdkg");
 
@@ -171,40 +246,119 @@ function loseHandler()
 	setStatistics(userStats);
 }
 
-function setSelectedLetterStyle(letterElement: HTMLElement)
+async function checkCurrentRow(): Promise<boolean | string>
+{
+	const word = getCurrentWord();
+
+	const text = await (await fetch(`/guess?word=${word}`, { method: "POST" })).text();
+
+	try
+	{
+		const specifiedWord = JSON.parse(text) as { type: letterType, _charCode: number }[];
+
+		let correctLetters = 0;
+
+		specifiedWord.forEach((value, index) =>
+		{
+			setSpecifiedLetterStyle(index, currentRow, value.type);
+			setKeyboardLetterStyle(toUpper(value._charCode), value.type);
+
+			if (value.type == "correct")
+			{
+				correctLetters++;
+			}
+		});
+
+		return correctLetters == lettersCount;
+	}
+	catch
+	{
+		return text;
+	}
+}
+
+function setSelectedLetterStyle(letterElement: HTMLElement): void
 {
 	letterElement.animate([
 		{ transform: "scale(1)" },
 		{ transform: "scale(1.1)" },
 		{ transform: "scale(1)" },
 	], {
-		duration: 200,
+		duration: 100,
 		iterations: 1
 	});
 
 	letterElement.style.borderColor = "var(--border-selected-unspecified)";
 }
 
-function setUnselectedLetterStyle(letterElement: HTMLElement)
+function setUnselectedLetterStyle(letterElement: HTMLElement): void
 {
 	letterElement.style.borderColor = "var(--border-unspecified)";
 }
 
+function setSpecifiedLetterStyle(index: number, row: number, type: letterType): void
+{
+	const letterElement = getLetter(index, row);
 
-function keyHandler(charCode: number)
+	if (letterElement)
+	{
+		switch (type)
+		{
+			case "correct":
+				letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedCorrect";
+				break;
+			case "present":
+				letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedPresent";
+				break;
+			case "absent":
+				letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedAbsent";
+				break;
+			default:
+				throw new Error("Unknown letter type");
+		}
+
+		letterElement.style.animationDelay = index * animationDelay + "s";
+	}
+}
+
+function setKeyboardLetterStyle(code: number, type: letterType)
+{
+	const key = getKey(code);
+
+	if (key)
+	{
+		switch (type)
+		{
+			case "correct":
+				key.style.background = "var(--background-specified-correct)";
+				break;
+			case "present":
+				key.style.background = "var(--background-specified-present)";
+				break;
+			case "absent":
+				key.style.background = "var(--background-specified-absent)";
+				break;
+			default:
+				throw new Error("Unknown letter type");
+		}
+	}
+}
+
+function keyHandler(charCode: number): void
 {
 	const char = String.fromCharCode(charCode);
 	const letterElement = getLetter(currentColumn, currentRow);
 
-	if (alphabet.test(char) && letterElement && (currentColumn != lettersCount - 1 || letterElement.innerHTML.length == 0)) //set letter
+	if (alphabet.test(char) && currentColumn < lettersCount && letterElement)
 	{
 		letterElement.innerHTML = char;
-		currentColumn = Math.min(currentColumn + 1, lettersCount - 1);
-		setSelectedLetterStyle(letterElement);		
+		currentColumn++;
+
+		setSelectedLetterStyle(letterElement);
 	}
-	else if (charCode == 13) //enter
+	else if (charCode == 13)
 	{
-		const win = submitCurrentRow();
+		const win = checkCurrentRow();
 
 		win.then((value) =>
 		{
@@ -230,7 +384,7 @@ function keyHandler(charCode: number)
 			}
 		})
 	}
-	else if (charCode == 8 && currentColumn - 1 >= 0 && letterElement && letterElement.innerHTML.length == 0) //backspace
+	else if (charCode == 8 && currentColumn - 1 > -1)
 	{
 		const editedElement = getLetter(currentColumn - 1, currentRow);
 
@@ -238,24 +392,18 @@ function keyHandler(charCode: number)
 		{
 			editedElement.innerHTML = "";
 			setUnselectedLetterStyle(editedElement);
-			currentColumn--;		
-		}		
-	}
-	else if (charCode == 8 && letterElement) //backspace last letter
-	{
-		letterElement.innerHTML = "";
-		setUnselectedLetterStyle(letterElement);
+			currentColumn--;
+		}
 	}
 }
 
 window.addEventListener("load", () =>
 {
 	document.body.appendChild(createBoard());
+	document.body.appendChild(createKeyboard());
 });
 
 window.addEventListener("keydown", (event) =>
 {
 	keyHandler(event.keyCode);
 });
-
-console.log("zzzzzzzz");
--- server/routes/app.js
+++ server/routes/app.js
@@ -14,20 +14,64 @@ const animationDelay = 0.2;
 const alphabet = /^[A-Za-z0-9]*$/;
 const statisticsId = "statistics";
 const guessesId = "guesses";
+const keyboardRow1 = [
+    { content: undefined, code: 81 },
+    { content: undefined, code: 87 },
+    { content: undefined, code: 69 },
+    { content: undefined, code: 82 },
+    { content: undefined, code: 84 },
+    { content: undefined, code: 89 },
+    { content: undefined, code: 85 },
+    { content: undefined, code: 73 },
+    { content: undefined, code: 79 },
+    { content: undefined, code: 80 }
+];
+const keyboardRow2 = [
+    { content: undefined, code: 65 },
+    { content: undefined, code: 83 },
+    { content: undefined, code: 68 },
+    { content: undefined, code: 70 },
+    { content: undefined, code: 71 },
+    { content: undefined, code: 72 },
+    { content: undefined, code: 74 },
+    { content: undefined, code: 75 },
+    { content: undefined, code: 76 }
+];
+const keyboardRow3 = [
+    { content: "ENTER", code: 13, flex: 1.5 },
+    { content: undefined, code: 90 },
+    { content: undefined, code: 88 },
+    { content: undefined, code: 67 },
+    { content: undefined, code: 68 },
+    { content: undefined, code: 66 },
+    { content: undefined, code: 78 },
+    { content: undefined, code: 77 },
+    { content: "@/backspace.svg", code: 8, flex: 1.5 }
+];
 let currentRow = 0;
 let currentColumn = 0;
+function toUpper(code) {
+    return String.fromCharCode(code).toUpperCase().charCodeAt(0);
+}
 function getLetterId(column, row) {
     return "letter" + column + "_" + row;
 }
 function getLetter(column, row) {
     return document.getElementById(getLetterId(column, row));
 }
+function getKeyId(code) {
+    return "key_" + code;
+}
+function getKey(code) {
+    return document.getElementById(getKeyId(code));
+}
 function createRow(row) {
     const container = document.createElement("div");
     container.className = "flex rowContainer";
+    container.style.width = "calc(var(--size) * 330)";
     for (let column = 0; column < lettersCount; column++) {
         const letterContainer = document.createElement("div");
-        letterContainer.className = "letterContainer unspecifiedLetter";
+        letterContainer.className = "innerCentralAlign letterContainer unspecifiedLetter";
         letterContainer.id = getLetterId(column, row);
         container.appendChild(letterContainer);
     }
@@ -36,6 +80,7 @@ function createRow(row) {
 function createBoard() {
     const container = document.createElement("div");
     container.className = "flex columnContainer";
+    container.style.height = "calc(var(--size) * 400)";
     for (let row = 0; row < rowsCount; row++) {
         container.appendChild(createRow(row));
     }
@@ -43,6 +88,47 @@ function createBoard() {
 }
 function createBlankStatistics() {
     return { correctAnswers: 0, totalAnswers: 0, guessesDistribution: new Array(rowsCount).fill(0), currentStreak: 0, maxStreak: 0 };
+}
+function createKey(content, action) {
+    const container = document.createElement("div");
+    container.className = "innerCentralAlign keyStyle";
+    if (content[0] == "@") {
+        container.innerHTML = `<img src="${content.substring(1)}"></img>`;
+    }
+    else {
+        container.innerHTML = content;
+    }
+    container.onclick = action;
+    return container;
+}
+function createKeyboardRow(row) {
+    const container = document.createElement("div");
+    container.className = "rowContainer";
+    container.style.display = "flex";
+    container.style.justifyContent = "center";
+    container.style.width = "calc(var(--key-size)*484)";
+    container.style.padding = "0 calc(var(--key-size)*8) 0 calc(var(--key-size)*8)";
+    row.forEach((value, index) => {
+        const key = createKey(value.content ? value.content : String.fromCharCode(value.code), () => { keyHandler(value.code); });
+        if (index < row.length - 1) {
+            key.style.marginRight = "calc(var(--size) * 6)";
+        }
+        if (value.flex) {
+            key.style.flex = value.flex.toString();
+        }
+        key.id = getKeyId(value.code);
+        container.appendChild(key);
+    });
+    return container;
+}
+function createKeyboard() {
+    const container = document.createElement("div");
+    container.className = "flex columnContainer";
+    container.style.height = "200px";
+    container.appendChild(createKeyboardRow(keyboardRow1));
+    container.appendChild(createKeyboardRow(keyboardRow2));
+    container.appendChild(createKeyboardRow(keyboardRow3));
+    return container;
 }
 function getStatistics() {
     const stats = localStorage.getItem(statisticsId);
@@ -58,45 +144,15 @@ function getStatistics() {
 function setStatistics(stats) {
     localStorage.setItem(statisticsId, JSON.stringify(stats));
 }
-function submitCurrentRow() {
-    return __awaiter(this, void 0, void 0, function* () {
-        let word = "";
-        for (let index = 0; index < lettersCount; index++) {
-            const letterElement = getLetter(index, currentRow);
-            if (letterElement) {
-                word += letterElement.innerHTML;
-            }
+function getCurrentWord() {
+    let word = "";
+    for (let index = 0; index < lettersCount; index++) {
+        const letterElement = getLetter(index, currentRow);
+        if (letterElement) {
+            word += letterElement.innerHTML;
         }
-        const text = yield (yield fetch(`/guess?word=${word}`, { method: "POST" })).text();
-        try {
-            const specifiedWord = JSON.parse(text);
-            let correctLetters = 0;
-            specifiedWord.forEach((value, index) => {
-                const letterElement = getLetter(index, currentRow);
-                if (letterElement) {
-                    switch (value.type) {
-                        case "correct":
-                            letterElement.className = "letterContainer specifiedLetter specifiedCorrect";
-                            correctLetters++;
-                            break;
-                        case "present":
-                            letterElement.className = "letterContainer specifiedLetter specifiedPresent";
-                            break;
-                        case "absent":
-                            letterElement.className = "letterContainer specifiedLetter specifiedAbsent";
-                            break;
-                        default:
-                            throw new Error("Unknown letter type");
-                    }
-                    letterElement.style.animationDelay = index * animationDelay + "s";
-                }
-            });
-            return correctLetters == lettersCount;
-        }
-        catch (_a) {
-            return text;
-        }
-    });
+    }
+    return word;
 }
 function winHandler() {
     alert("biungo");
@@ -115,13 +171,34 @@ function loseHandler() {
     userStats.currentStreak = 0;
     setStatistics(userStats);
 }
+function checkCurrentRow() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const word = getCurrentWord();
+        const text = yield (yield fetch(`/guess?word=${word}`, { method: "POST" })).text();
+        try {
+            const specifiedWord = JSON.parse(text);
+            let correctLetters = 0;
+            specifiedWord.forEach((value, index) => {
+                setSpecifiedLetterStyle(index, currentRow, value.type);
+                setKeyboardLetterStyle(toUpper(value._charCode), value.type);
+                if (value.type == "correct") {
+                    correctLetters++;
+                }
+            });
+            return correctLetters == lettersCount;
+        }
+        catch (_a) {
+            return text;
+        }
+    });
+}
 function setSelectedLetterStyle(letterElement) {
     letterElement.animate([
         { transform: "scale(1)" },
         { transform: "scale(1.1)" },
         { transform: "scale(1)" },
     ], {
-        duration: 200,
+        duration: 100,
         iterations: 1
     });
     letterElement.style.borderColor = "var(--border-selected-unspecified)";
@@ -129,18 +206,53 @@ function setSelectedLetterStyle(letterElement) {
 function setUnselectedLetterStyle(letterElement) {
     letterElement.style.borderColor = "var(--border-unspecified)";
 }
+function setSpecifiedLetterStyle(index, row, type) {
+    const letterElement = getLetter(index, row);
+    if (letterElement) {
+        switch (type) {
+            case "correct":
+                letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedCorrect";
+                break;
+            case "present":
+                letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedPresent";
+                break;
+            case "absent":
+                letterElement.className = "innerCentralAlign letterContainer specifiedLetter specifiedAbsent";
+                break;
+            default:
+                throw new Error("Unknown letter type");
+        }
+        letterElement.style.animationDelay = index * animationDelay + "s";
+    }
+}
+function setKeyboardLetterStyle(code, type) {
+    const key = getKey(code);
+    if (key) {
+        switch (type) {
+            case "correct":
+                key.style.background = "var(--background-specified-correct)";
+                break;
+            case "present":
+                key.style.background = "var(--background-specified-present)";
+                break;
+            case "absent":
+                key.style.background = "var(--background-specified-absent)";
+                break;
+            default:
+                throw new Error("Unknown letter type");
+        }
+    }
+}
 function keyHandler(charCode) {
     const char = String.fromCharCode(charCode);
     const letterElement = getLetter(currentColumn, currentRow);
-    if (alphabet.test(char) && letterElement && (currentColumn != lettersCount - 1 || letterElement.innerHTML.length == 0)) //set letter
-     {
+    if (alphabet.test(char) && currentColumn < lettersCount && letterElement) {
         letterElement.innerHTML = char;
-        currentColumn = Math.min(currentColumn + 1, lettersCount - 1);
+        currentColumn++;
         setSelectedLetterStyle(letterElement);
     }
-    else if (charCode == 13) //enter
-     {
-        const win = submitCurrentRow();
+    else if (charCode == 13) {
+        const win = checkCurrentRow();
         win.then((value) => {
             if (typeof value == "boolean") {
                 if (value) {
@@ -159,8 +271,7 @@ function keyHandler(charCode) {
             }
         });
     }
-    else if (charCode == 8 && currentColumn - 1 >= 0 && letterElement && letterElement.innerHTML.length == 0) //backspace
-     {
+    else if (charCode == 8 && currentColumn - 1 > -1) {
         const editedElement = getLetter(currentColumn - 1, currentRow);
         if (editedElement) {
             editedElement.innerHTML = "";
@@ -168,16 +279,11 @@ function keyHandler(charCode) {
             currentColumn--;
         }
     }
-    else if (charCode == 8 && letterElement) //backspace last letter
-     {
-        letterElement.innerHTML = "";
-        setUnselectedLetterStyle(letterElement);
-    }
 }
 window.addEventListener("load", () => {
     document.body.appendChild(createBoard());
+    document.body.appendChild(createKeyboard());
 });
 window.addEventListener("keydown", (event) => {
     keyHandler(event.keyCode);
 });
-console.log("zzzzzzzz");
